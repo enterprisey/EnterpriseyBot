2bbaf4022b39721a0d7e7dab8a591358d2e93aa0
APerson <setup.pyc@gmail.com>
2014-06-07 18:03:37 -0400

    added things

--- dyknotifier/dyknotifier.py
+++ dyknotifier/dyknotifier.py
@@ -13,6 +13,7 @@ class DYKNotifier():
     def __init__(self):
         self._wiki = Wiki("http://en.wikipedia.org/w/api.php")
         self._ttdyk = Page(self._wiki, title="Template talk:Did you know")
+        self._dyk_noms = self.get_list_of_dyk_noms_from_ttdyk()
 
     def get_list_of_dyk_noms_from_ttdyk(self):
         """
@@ -33,64 +34,59 @@ class DYKNotifier():
                 dyk_noms.append(template["*"])
         return dyk_noms
 
-    def prune_resolved_dyk_noms(self, dyk_noms):
-        print "About to prune a list of " + str(len(dyk_noms)) + " DYK noms."
-        dyk_noms_strings = list_to_pipe_separated_query(dyk_noms)
-        eventual_count = (len(dyk_noms) // 50) + (cmp(len(dyk_noms), 0))
+    def prune_dyk_noms(self, should_prune):
+        """
+        Prune the list of DYK noms given a function which determines whether
+        a given page should be removed.
+        """
+        print "About to prune a list of " + str(len(self._dyk_noms)) + " DYK noms."
+        dyk_noms_strings = list_to_pipe_separated_query(self._dyk_noms)
+        eventual_count = (len(self._dyk_noms) // 50) + (cmp(len(self._dyk_noms), 0))
         count = 1
         removed_count = 0
         for dyk_noms_string in dyk_noms_strings:
-            params = {"action":"query", "titles":dyk_noms_string, "prop":\
-                      "categories"}
+            params = {"action":"query", "titles":dyk_noms_string}
+            # Based on how the function is pruning, complete the rest of the params
+            if should_prune is self.should_prune_as_resolved:
+                # This function uses categories, so querying for categories
+                params["prop"] = "categories"
+            elif should_prune is self.should_prune_as_self_nom:
+                # This function uses wikitext, so querying for wikitext
+                params["prop"] = "revisions"
+                params["rvprop"] = "content"
             api_request = api.APIRequest(self._wiki, params)
             api_result = api_request.query()
             print "Processing results from query number " + str(count) +\
                   " out of " + str(eventual_count) + "..."
             for page in api_result["query"]["pages"].values():
-                if not "categories" in page:
-                    continue
-                for category in page["categories"]:
-                    if "Category:Passed DYK nominations" in category["title"]\
-                       or "Category:Failed DYK nominations" in\
-                       category["title"]:
-                        # Since it's already been passed or failed,
-                        # let's not notify the creator
-                        dyk_noms.remove(page["title"])
-                        removed_count += 1
+                if should_prune(page):
+                    self._dyk_noms.remove(page["title"])
+                    removed_count += 1
             count += 1
-        print "Removed " + str(removed_count) + " noms because they passed" +\
-              " or failed. " + str(len(dyk_noms)) + " left in the list."
-        return dyk_noms
+        print "Removed " + str(removed_count) + " noms. " +\
+              str(len(self._dyk_noms)) + " left in the list."
 
-    def remove_self_noms(self, dyk_noms):
-        print "Removing self-nominations from the list of DYK noms."
-        removed_count = 0
-        for dyk_nom_title in dyk_noms[:]:
-            print "Checking " + dyk_nom_title + "..."
-            dyk_nom = Page(self._wiki, title=dyk_nom_title)
-            nom_wikitext = dyk_nom.getWikiText()
-            self_nom = re.search("Self nominated at ", nom_wikitext)
-            if self_nom:
-                dyk_noms.remove(dyk_nom_title)
-                removed_count += 1
-        print "Removed " + str(removed_count) + " noms because self-nom. " +\
-              str(len(dyk_noms)) + " left in the list."
-        return dyk_noms
-            
+    def should_prune_as_resolved(self, page):
+        """
+        Given a page, should it be pruned from the list of DYK noms
+        since it's already been passed or failed?
+        """
+        if not "categories" in page:
+            return False
+        for category in page["categories"]:
+            if "Category:Passed DYK nominations" in category["title"] or\
+               "Category:Failed DYK nominations" in category["title"]:
+                return True
+        return False
+
+    def should_prune_as_self_nom(self, page):
+        wikitext = page["revisions"][0]["*"]
+        print wikitext[:100]
+        return False            
                 
-    def get_people_to_notify(self, dyk_noms):
+    def get_people_to_notify(self):
         people_to_notify = []
-        for dyk_nom_title in dyk_noms:
-            print "Getting who to notify for " + dyk_nom_title + "..."
-            dyk_nom = Page(self._wiki, title=dyk_nom_title)
-            nom_wikitext = dyk_nom.getWikiText()
-            line_matchobj = re.search(r"<small>\S*", wikitext)
-            line_begin = nom_wikitext.find("<small>Created by ")
-            line_end = nom_wikitext[line_begin:].find(\
-                "</small>.")
-            line_begin += len("<small>")
-            line_end += line_begin - 7 # To take out "</small>."
-            print nom_wikitext[line_begin:line_end]
+        
         return people_to_notify
 
 def pretty_print(query_result):
@@ -115,11 +111,9 @@ def main():
     print "[main()] Before DYKNotifier constructor"
     notifier = DYKNotifier()
     print "[main()] Constructed a DYKNotifier."
-    dyk_noms = notifier.get_list_of_dyk_noms_from_ttdyk()
-    print "[main()] Got a list of DYK noms from T:TDYK."
-    dyk_noms = notifier.prune_resolved_dyk_noms(dyk_noms)
+    notifier.prune_dyk_noms(notifier.should_prune_as_resolved)
     print "[main()] Pruned resolved noms from the list of DYK noms."
-    dyk_noms = notifier.remove_self_noms(dyk_noms)
+    notifier.prune_dyk_noms(notifier.should_prune_as_self_nom)
     print "[main()] Removed self-noms from the list of DYK noms."
     #people_to_notify = notifier.get_people_to_notify(dyk_noms)
     #print "[main()] Got a list of people to notify."
