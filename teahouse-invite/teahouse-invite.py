e4959cd18c15fed27f852e5c8f0324c0f917211a
APerson <danielhglus@gmail.com>
2015-08-18 21:31:14 -0400

    [teahouse-invite] Update for actual operation

--- teahouse-invite/teahouse-invite.py
+++ teahouse-invite/teahouse-invite.py
@@ -1,37 +1,59 @@
+import argparse
 import re
 
 from clint.textui import progress
 import pywikibot
 import pywikibot.pagegenerators as pagegenerator
 
-COMMENT = "Testing an experimental fixer for Teahouse invites"
+COMMENT = "[[Wikipedia:Bots/Requests for approval/APersonBot 3|Bot]] fixing duplicated Teahouse invitations"
 CATEGORY = "Category:Wikipedians who have received a Teahouse invitation through AfC"
 CATEGORIES = (CATEGORY, "Category:Wikipedians who have received a Teahouse invitation")
+
 WIKI = pywikibot.Site("en", "wikipedia")
 WIKI.login()
 
-def main():
-    fix_page(raw_input("Page title: "))
+# Load the template
+with open("template", "r") as template_file:
+    INVITE_PATTERN = re.compile(template_file.read())
 
-def fix_page(page_title):
-    page = pywikibot.Page(WIKI, title=page_title)
+# Parse the arguments
+parser = argparse.ArgumentParser(prog="teahouse-invite",
+                                 description="Fix duplicated Teahouse invites.")
+parser.add_argument("-p", "--page", type=str,
+                    help="A specific page to process.")
+args = parser.parse_args()
+
+edits_made = [0]
+pages_scanned = 0
+user_talk_pages = pagegenerator.AllpagesPageGenerator(namespace=3, site=WIKI,
+                                                      content=True,
+                                                      includeredirects=False)
+preloaded_pages = pagegenerator.PreloadingGenerator(user_talk_pages)
+for page in preloaded_pages if not args.page else [pywikibot.Page(WIKI, args.page)]:
     page_text = page.get()
-    page_text = fix_page_text(page_text)
-    if page_text:
-        if not any(x in page_text for x in CATEGORIES):
-            page_text = page_text + "\n\n[[" + CATEGORY + "]]"
-        page.save(text=page_text, comment=COMMENT)
 
-def fix_page_text(page_text):
-    with open("template", "r") as template_file:
-        INVITE_PATTERN = re.compile(template_file.read())
+    pages_scanned += 1
+    if (pages_scanned % 100) == 0:
+        print("Scanned %d pages." % pages_scanned)
+
+    # Fix page text
     matches = INVITE_PATTERN.findall(page_text)
-    if not matches:
-        return None
+    if not matches or len(matches) == 1:
+        continue
 
+    print("FOUND ONE: %s" % page.title(withNamespace=True))
     for match in matches[1:]:
-        page_text = page_text.replace(match, "")
-    return page_text
+        page_text = page_text.replace(match[0], "")
 
-if __name__=="__main__":
-    main()
+    # Add a maintenance category, if there isn't already one
+    if not any(x in page_text for x in CATEGORIES):
+        page_text = page_text + "\n\n[[" + CATEGORY + "]]"
+
+    def page_save_callback(the_page, exception):
+
+        # The "exception" argument is None if the edit was successful
+        if not exception:
+            edits_made[0] = edits_made[0] + 1
+            print("We've made %d edits." % edits_made[0])
+
+    page.save(text=page_text, comment=COMMENT, callback=page_save_callback)
