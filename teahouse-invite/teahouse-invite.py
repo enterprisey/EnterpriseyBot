32e0ddb3cfcd6734339353df2d99f9f7fe37207a
APerson <danielhglus@gmail.com>
2015-08-22 21:27:30 +0000

    added persistent state and error handling

--- teahouse-invite/teahouse-invite.py
+++ teahouse-invite/teahouse-invite.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import re
 
 from clint.textui import progress
@@ -8,12 +9,13 @@ import pywikibot.pagegenerators as pagegenerator
 COMMENT = "[[Wikipedia:Bots/Requests for approval/APersonBot 3|Bot]] fixing duplicated Teahouse invitations"
 CATEGORY = "Category:Wikipedians who have received a Teahouse invitation through AfC"
 CATEGORIES = (CATEGORY, "Category:Wikipedians who have received a Teahouse invitation")
+STATE_FILE = "saved_state.json"
 
 WIKI = pywikibot.Site("en", "wikipedia")
 WIKI.login()
 
 # Load the template
-with open("template", "r") as template_file:
+with open("invite-template.txt", "r") as template_file:
     INVITE_PATTERN = re.compile(template_file.read())
 
 # Parse the arguments
@@ -23,37 +25,60 @@ parser.add_argument("-p", "--page", type=str,
                     help="A specific page to process.")
 args = parser.parse_args()
 
-edits_made = [0]
+# Load the saved state
+with open(STATE_FILE) as state_file:
+    saved_state = json.load(state_file)
+    edits_made = [saved_state["edits_made"]]
+    current_username = saved_state["current_username"]
+
 pages_scanned = 0
 user_talk_pages = pagegenerator.AllpagesPageGenerator(namespace=3, site=WIKI,
                                                       content=True,
-                                                      includeredirects=False)
+                                                      includeredirects=False,
+                                                      start=current_username)
 preloaded_pages = pagegenerator.PreloadingGenerator(user_talk_pages)
 for page in preloaded_pages if not args.page else [pywikibot.Page(WIKI, args.page)]:
-    page_text = page.get()
+    try:
+        page_text = page.get()
 
-    pages_scanned += 1
-    if (pages_scanned % 100) == 0:
-        print("Scanned %d pages." % pages_scanned)
+        pages_scanned += 1
+        if (pages_scanned % 100) == 0:
+            print("Scanned %d pages." % pages_scanned)
+            if (pages_scanned % 1000) == 0:
+                print("Current page: %s; edits made: %d" % (page.title(withNamespace=True), edits_made[0]))
+                if (pages_scanned % 10000) == 0:
+                    with open(STATE_FILE, "w") as state_file:
+                        current_state = {"edits_made": edits_made[0],
+                                         "current_username": page.title(withNamespace=False)}
+                        json.dump(current_state, state_file)
+                        print("Dumped state to %s." % state_file)
 
-    # Fix page text
-    matches = INVITE_PATTERN.findall(page_text)
-    if not matches or len(matches) == 1:
-        continue
+        # Fix page text
+        matches = INVITE_PATTERN.findall(page_text)
+        if not matches or len(matches) == 1:
+            continue
 
-    print("FOUND ONE: %s" % page.title(withNamespace=True))
-    for match in matches[1:]:
-        page_text = page_text.replace(match[0], "")
+        for match in matches[1:]:
+            page_text = page_text.replace(match[0], "")
 
-    # Add a maintenance category, if there isn't already one
-    if not any(x in page_text for x in CATEGORIES):
-        page_text = page_text + "\n\n[[" + CATEGORY + "]]"
+        # Add a maintenance category, if there isn't already one
+        if not any(x in page_text for x in CATEGORIES):
+            page_text = page_text + "\n\n[[" + CATEGORY + "]]"
 
-    def page_save_callback(the_page, exception):
+        def page_save_callback(_, exception):
 
-        # The "exception" argument is None if the edit was successful
-        if not exception:
-            edits_made[0] = edits_made[0] + 1
-            print("We've made %d edits." % edits_made[0])
+            # The "exception" argument is None if the edit was successful
+            if not exception:
+                edits_made[0] = edits_made[0] + 1
+                print("We've made %d edits." % edits_made[0])
 
-    page.save(text=page_text, comment=COMMENT, callback=page_save_callback)
+        page.save(text=page_text, comment=COMMENT, callback=page_save_callback)
+    except:
+        current_username = page.title(withNamespace=False)
+        print("Leaving off at '%s'" % current_username)
+        with open(STATE_FILE, "w") as state_file:
+            current_state = {"edits_made": edits_made[0],
+                             "current_username": current_username}
+            json.dump(current_state, state_file)
+            print("Dumped state.")
+        raise
