949837bf1e4cfe9c5c7eee963433c49dccbbebd7
APerson <danielhglus@gmail.com>
2015-11-04 23:23:53 -0500

    Forget state

--- teahouse-invite/teahouse-invite.py
+++ teahouse-invite/teahouse-invite.py
@@ -1,15 +1,12 @@
 import argparse
-import json
 import re
 
-from clint.textui import progress
 import pywikibot
 import pywikibot.pagegenerators as pagegenerator
 
 COMMENT = "[[Wikipedia:Bots/Requests for approval/APersonBot 3|Bot]] fixing duplicated Teahouse invitations"
 CATEGORY = "Category:Wikipedians who have received a Teahouse invitation through AfC"
 CATEGORIES = (CATEGORY, "Category:Wikipedians who have received a Teahouse invitation")
-STATE_FILE = "saved_state.json"
 
 WIKI = pywikibot.Site("en", "wikipedia")
 WIKI.login()
@@ -25,15 +22,8 @@ parser.add_argument("-p", "--page", type=str,
                     help="A specific page to process.")
 args = parser.parse_args()
 
-# Load the saved state
-with open(STATE_FILE) as state_file:
-    saved_state = json.load(state_file)
-    edits_made = [saved_state["edits_made"]]
-    current_username = saved_state["current_username"]
-
-print("Starting on %s, with %d edits made." % (current_username, edits_made[0]))
-
 pages_scanned = 0
+edits_made = 0
 teahouse_img_pages = pywikibot.FilePage(WIKI, "File:WP teahouse logo 2.png").usingPages()
 for page in teahouse_img_pages if not args.page else [pywikibot.Page(WIKI, args.page)]:
     try:
@@ -46,13 +36,7 @@ for page in teahouse_img_pages if not args.page else [pywikibot.Page(WIKI, args.
         if (pages_scanned % 100) == 0:
             print("Scanned %d pages." % pages_scanned)
             if (pages_scanned % 1000) == 0:
-                print("Current page: %s; edits made: %d" % (page.title(withNamespace=True), edits_made[0]))
-                if (pages_scanned % 10000) == 0:
-                    with open(STATE_FILE, "w") as state_file:
-                        current_state = {"edits_made": edits_made[0],
-                                         "current_username": page.title(withNamespace=False)}
-                        json.dump(current_state, state_file)
-                        print("Dumped state to %s." % STATE_FILE)
+                print("Current page: %s; edits made: %d" % (page.title(withNamespace=True), edits_made))
 
         # Fix page text
         matches = INVITE_PATTERN.findall(page_text)
@@ -70,16 +54,11 @@ for page in teahouse_img_pages if not args.page else [pywikibot.Page(WIKI, args.
 
             # The "exception" argument is None if the edit was successful
             if not exception:
-                edits_made[0] = edits_made[0] + 1
-                print("We've made %d edits." % edits_made[0])
+                edits_made = edits_made + 1
+                print("We've made %d edits." % edits_made)
 
         page.save(text=page_text, comment=COMMENT, callback=page_save_callback)
     except:
         current_username = page.title(withNamespace=False)
         print("Leaving off at '%s'" % current_username)
-        with open(STATE_FILE, "w") as state_file:
-            current_state = {"edits_made": edits_made[0],
-                             "current_username": current_username}
-            json.dump(current_state, state_file)
-            print("Dumped state.")
         raise
