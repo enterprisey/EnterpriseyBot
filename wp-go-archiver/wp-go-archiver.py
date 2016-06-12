a52b489befcdc38d8f87b67f4b81db638f972e91
APerson <danielhglus@gmail.com>
2016-06-12 13:46:39 -0400

    [wp-go-archiver] Fix #5

--- wp-go-archiver/wp-go-archiver.py
+++ wp-go-archiver/wp-go-archiver.py
@@ -5,8 +5,9 @@ import sys
 
 WP_GO_TITLE = "Wikipedia:Goings-on"
 DATE_REGEX = r"\[\[(\w+ \d{1,2})\]\], \[\[(\d{4})\]\]"
-CURRENT_ITEM = r"\*\s?[\s\S]+?\(\d{1,2} \w{3}\)\n"
+CURRENT_ITEM = r"\*\s?[\s\S]+?\(\d{1,2} \w+?\)\n"
 HATNOTE = "{{redirect|WP:GO|the Go button|Help:Searching|the Go WikiProject|Wikipedia:WikiProject Go}}"
+ADVERTISEMENT = " ([[Wikipedia:Bots/Requests for approval/APersonBot 6|Bot]])"
 
 print("Starting dyknotifier at " + datetime.datetime.utcnow().isoformat())
 
@@ -30,14 +31,15 @@ if date_on_page == new_date.strftime("%B %-d"):
 previous_date = ", ".join(re.search(DATE_REGEX, current_text).groups())
 archive_title = WP_GO_TITLE + "/" + previous_date
 print("Archiving to {}".format(archive_title))
-wp_go.move(archive_title, reason="archive", movetalkpage=False)
+wp_go.text = wp_go.text.replace(HATNOTE, "").strip()
+wp_go.save(summary="Stripping hatnote before archival" + ADVERTISEMENT)
+wp_go.move(archive_title, reason="Archive" + ADVERTISEMENT, movetalkpage=False)
 
 # Create a new page with the updated text
 new_text = current_text
 new_date_text = new_date.strftime("[[%B %-d]], [[%Y]]")
 new_text = re.sub(DATE_REGEX, new_date_text, new_text)
 new_text = re.sub(CURRENT_ITEM, "", new_text)
-new_text = new_text.replace(HATNOTE, "")
 
 wp_go.text = new_text
-wp_go.save(summary="New week ([[User talk:APerson|Bot]])")
+wp_go.save(summary="New week" + ADVERTISEMENT)
